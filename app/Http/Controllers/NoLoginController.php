51cd0b5555fae3bfbb3f6d699809635e50c588e5
dareekun <dareekun@live.com>
2021-05-24 07:42:16 +0700

    Update NoLoginController.php
    remove un used dependencies

--- app/Http/Controllers/NoLoginController.php
+++ app/Http/Controllers/NoLoginController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Exports\UsersExport;
-use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Storage;
 
 class NoLoginController extends Controller
