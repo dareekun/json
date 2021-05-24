dc81305bc2618a25464e72dfda1cd3ec1d5b9c20
dareekun <dareekun@live.com>
2021-05-24 07:41:17 +0700

    Update NoLoginController.php
    Fix Schedulle, and fix download button

--- app/Http/Controllers/NoLoginController.php
+++ app/Http/Controllers/NoLoginController.php
@@ -20,20 +20,33 @@ class NoLoginController extends Controller
     }
 
     public function download(){
-        return (new UsersExport)->download('CARDAT'.date('Ymd').'.SDF', \Maatwebsite\Excel\Excel::CSV);
+        		$name = 'CARDAT'.date('Ymd').'.SDF';
+				$file = fopen('../storage/app/public/Data/'.$name, "a") or die("Unable to open bell times file!");
+				$start = DB::table('option')->where('name', 'start')->select('time')->value('time');
+				$stop  = DB::table('option')->where('name', 'stop')->select('time')->value('time');
+				$today   = date('Y-m-d H:i:s', strtotime($stop));
+				$yesterday = date('Y-m-d H:i:s', strtotime($start.' - 1 days'));
+				$record = DB::table('data')->whereDate('waktu', '>', $yesterday)->whereDate('waktu', '<', $today)->get();
+				foreach ($record as $rc) {
+					$txt = '00'.$rc->nik.date('Hi', strtotime($rc->waktu)).date('md', strtotime($rc->waktu));
+					fwrite($file, $txt. "\n");
+				}
+				fclose ($file);
+				return response()->download('../storage/app/public/Data/'.$name);
     }
 
     public function schedule(){
-        $array = [];
-        $start = DB::table('option')->where('name', 'start')->select('time')->value('time');
-        $stop  = DB::table('option')->where('name', 'stop')->select('time')->value('time');
-        $today   = date('Y-m-d H:i:s', strtotime($stop));
-        $yesterday = date('Y-m-d H:i:s', strtotime($start.' - 1 days'));
-        $record = DB::table('data')->whereDate('waktu', '>', $yesterday)->whereDate('waktu', '<', $today)->get();
-        foreach ($record as $rc) {
-            $array[] = '00'.$rc->nik.date('Hi', strtotime($rc->waktu)).date('md', strtotime($rc->waktu));
-        }
-        $data = collect($array)->transpose()->toArray();
-        Storage::disk('data')->put('CARDAT'.date('Ymd').'.SDF', $data);
+        		$name = 'SCH_CARDAT'.date('Ymd').'.SDF';
+				$file = fopen('../storage/app/public/Data/'.$name, "a") or die("Unable to open bell times file!");
+				$start = DB::table('option')->where('name', 'start')->select('time')->value('time');
+				$stop  = DB::table('option')->where('name', 'stop')->select('time')->value('time');
+				$today   = date('Y-m-d H:i:s', strtotime($stop));
+				$yesterday = date('Y-m-d H:i:s', strtotime($start.' - 1 days'));
+				$record = DB::table('data')->whereDate('waktu', '>', $yesterday)->whereDate('waktu', '<', $today)->get();
+				foreach ($record as $rc) {
+					$txt = '00'.$rc->nik.date('Hi', strtotime($rc->waktu)).date('md', strtotime($rc->waktu));
+					fwrite($file, $txt. "\n");
+				}
+				fclose ($file);
     }
 }
